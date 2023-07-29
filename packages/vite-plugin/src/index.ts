a944929b8786256bc2a7343418e9bfadc0d22d52
Luis Meyer <luis_meyer@outlook.de>
2023-07-29 19:25:56 +0200

    feat: add vite plugin to disable logging and dev server

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -6,39 +6,52 @@ let stopTlfcDev: () => Promise<void> | undefined;
 
 type VitePluginTlfcOptions = {
   includeAwsSdk?: boolean;
+  quiet?: boolean;
+  disableDevServer?: boolean;
 };
+
+async function startTlfcDev(quiet?: boolean) {
+  if (stopTlfcDev) {
+    await stopTlfcDev();
+  }
+
+  stopTlfcDev = await dev({ quiet });
+}
 
 export function vitePluginTlfc(
   options: VitePluginTlfcOptions | undefined
 ): Plugin {
-  const { includeAwsSdk } = options || {};
+  const { includeAwsSdk, disableDevServer, quiet } = options || {};
 
   return {
     name: "vite-plugin-tlfc",
     enforce: "pre",
 
     configureServer() {
-      // don't block vite from restarting
-      new Promise(async (resolve) => {
-        if (stopTlfcDev) {
-          await stopTlfcDev();
-        }
+      if (disableDevServer) {
+        return;
+      }
 
-        stopTlfcDev = await dev();
+      // don't await so we don't block vite from starting
+      startTlfcDev(quiet);
+    },
 
-        resolve(true);
-      });
+    configurePreviewServer() {
+      if (disableDevServer) {
+        return;
+      }
+
+      // don't await so we don't block vite from starting
+      startTlfcDev(quiet);
     },
 
     async transform(code, id) {
-      const isNodeModule = id.includes("node_modules");
-
       // omit aws-sdk from the bundle
       if (!includeAwsSdk && id.includes("@aws-sdk/client-lambda")) {
         return "export default null";
       }
 
-      if (isNodeModule) {
+      if (id.includes("node_modules")) {
         return;
       }
 
