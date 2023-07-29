4de224b7953e9c7388f8498c78d3e24bbd044c2b
Luis Meyer <luis_meyer@outlook.de>
2023-07-29 18:09:03 +0200

    feat: add deploy cmd to deploy aws stack without interacting with cdk

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 import { Command } from "commander";
 
+import { exec } from "child_process";
+
 import { buildStack, dev } from "@tlfc/tools";
 
 const program = new Command();
@@ -22,12 +24,26 @@ program
     await dev(entries);
   });
 
+const BuildStackCommand = "buildStack";
+
 program
-  .command("build")
+  .command(BuildStackCommand)
   .description("Build cdk stack.")
   .option("-e, --entries [files...]", "specify entry files")
   .action(async ({ entries }: CommandOptions) => {
     await buildStack(entries);
+  });
+
+program
+  .command("deploy")
+  .description("Deploying cdk stack.")
+  .action(() => {
+    const child = exec(
+      `cdk deploy --require-approval 'never' --app 'tlfc ${BuildStackCommand}'`
+    );
+
+    child.stdout?.pipe(process.stdout);
+    child.stderr?.pipe(process.stderr);
   });
 
 program.parse();
